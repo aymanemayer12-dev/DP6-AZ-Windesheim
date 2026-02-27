818c040592a139e8c35dfd95ea1ee0bb968f8329
s1504772 <ayman.zeroual@windesheim.nl>
2026-02-27 16:42:09 +0100

    week 2 dp6 ingredient.py constructor en getter aangepast voor kcal per eenheid

--- receptenboek/ingredient.py
+++ receptenboek/ingredient.py
@@ -1,8 +1,9 @@
 class Ingredient:
-    def __init__(self, naam, hoeveelheid, eenheid):
+    def __init__(self, naam, hoeveelheid, eenheid, kcal):
         self.__naam = naam
         self.__hoeveelheid = hoeveelheid
         self.__eenheid = eenheid
+        self.__kcal = kcal
 
     def get_naam(self):
         return self.__naam
@@ -12,6 +13,9 @@ class Ingredient:
 
     def get_eenheid(self):
         return self.__eenheid
+
+    def get_kcal(self):
+        return self.__kcal
 
     def __str__(self):
         return f"{self.__hoeveelheid} {self.__eenheid} {self.__naam}"
