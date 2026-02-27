c8e4a2e195d2679d4e44d937069eb9455d2d1fad
s1504772 <ayman.zeroual@windesheim.nl>
2026-02-27 14:41:42 +0100

    class recept uitgewerkt voor week 1 recept.py

--- receptenboek/recept.py
+++ receptenboek/recept.py
@@ -5,5 +5,23 @@ class Recept:
         self.__ingredient_list = []
         self.__stappen = []
 
-    # etc.. etc..
-    # Succes!
+    def get_naam(self):
+        return self.__naam
+
+    def voeg_ingredient_toe(self, ingredient):
+        self.__ingredient_list.append(ingredient)
+
+    def voeg_stap_toe(self, stap):
+        self.__stappen.append(stap)
+
+    def toon_recept(self):
+        print(f"\n=== {self.__naam} ===")
+        print(self.__omschrijving)
+
+        print("\nIngrediënten:")
+        for ingredient in self.__ingredient_list:
+            print(f"- {ingredient}")
+
+        print("\nBereidingsstappen:")
+        for i, stap in enumerate(self.__stappen, start=1):
+            print(f"{i}. {stap}")
